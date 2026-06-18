e5c3eb837e41720592406b4fb23e6b0ef9326c61
H-vishwa <130923885+H-vishwa@users.noreply.github.com>
2026-06-18 16:16:47 +0530

    Updated the Server

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,8 @@ app.use('/api/user',userRouter)
 app.use('/api/owner',ownerRouter)
 app.use('/api/bookings',bookingRouter)
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 export default app;
